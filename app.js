baefe19e11c85df437b88c2fb7cad824b299e37d
SHAHZAIB KHAN <56146605+Shahzaibsabir@users.noreply.github.com>
2023-11-17 21:49:41 +0500

    Update app.js
    add second number

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+let hour = document.querySelector(".hour")
+let mint = document.querySelector(".mint")
+let sec = document.querySelector(".sec")
+let num = 0;
+
+
+
+let second = setInterval(()=> sec.textContent = num++, 1000)
+
+if(sec==5){
+    sec.textContent = 0
+
+
+}
+
+console.log(sec)
 
