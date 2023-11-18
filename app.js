c56d0ba386dd9264d06be34f64b814bcd4f6e3cc
SHAHZAIB KHAN <56146605+Shahzaibsabir@users.noreply.github.com>
2023-11-18 23:01:42 +0500

    Update app.js
    Adding Hours  functionality

--- app.js
+++ app.js
@@ -15,3 +15,22 @@ if(sec==5){
 
 console.log(sec)
 
+
+
+let hour = document.querySelector(".hour")
+let mint = document.querySelector(".mint")
+let sec = document.querySelector(".sec")
+let num = 0;
+
+
+
+let second = setInterval(()=> sec.textContent = num++, 1000)
+
+if(sec==5){
+    sec.textContent = 0
+
+
+}
+
+console.log(sec)
+
